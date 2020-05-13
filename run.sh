3db35f87e3026257aea22a16e62fbde2b75b20ef
Adam Montlake <montlake.a@gmail.com>
2020-05-13 10:33:15 +0000

    add spacing

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ rm lex.yy.c  olympics.tab.c  olympics.tab.h olympics.c
 
 flex olymp.lex
 bison -d olympics.y
-gcc -o olympics.c olympics.tab.c lex.yy.c
+gcc  olympics.tab.c lex.yy.c -o olympics.c 
 ./olympics input.txt
 
